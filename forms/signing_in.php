bed8856c52aac3b4295151db392e394bb913acc0
Natalia Mwangi <natalimary2607@gmail.com>
2023-10-27 20:40:33 +0300

    sign in process

--- forms/signing_in.php
+++ forms/signing_in.php
@@ -1,41 +1,29 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+require_once "../sql/dbconn.php";
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+$email = $_POST["email"];
+$password = $_POST["password"];
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+// Prepare the SQL statement
+$select_data = "SELECT * FROM user WHERE email_address = '$email'";
 
-  $signing_in = new PHP_Email_Form;
-  $signing_in->ajax = true;
-  
-  $signing_in->to = $receiving_email_address;
-  $signing_in->from_email = $_POST['email'];
-  $signing_in->from_password = $_POST['password'];
-  $signing_in->subject = "New sign in request from the website";
+// Execute the query
+$result = $conn->query($select_data);
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $signing_in->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+if ($result->num_rows > 0) {
+    // Fetch the row as an associative array
+    $row = $result->fetch_assoc();
 
-  $signing_in->add_message( $_POST['email'], 'Email');
-  $signing_in->add_message( $_POST['password'], 'Password');
-
-
-  echo $signing_in->send();
+    // Verify the password
+    if (password_verify($password, $row['password'])) {
+        // Password is correct
+        print "Login successful!\n";
+    } else {
+        // Password is incorrect
+        print "Incorrect password\n";
+    }
+} else {
+    // No matching email found
+    echo "Email not found";
+}
 ?>
