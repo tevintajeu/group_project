c0ad7604b0aa468712451dbccd9eb5b93098f8e2
FatmaAgil <fatma.agil@strathmore.edu>
2023-10-27 21:31:12 +0300

    sign-in.php update

--- forms/signing_in.php
+++ forms/signing_in.php
@@ -1,29 +1,41 @@
+
 <?php
-require_once "../sql/dbconn.php";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-$email = $_POST["email"];
-$password = $_POST["password"];
+    $db_host = 'localhost';
+    $db_name = 'recipe';
+    $db_user = 'root';
+    $db_pass = ''; // Replace with your actual database password
 
-// Prepare the SQL statement
-$select_data = "SELECT * FROM user WHERE email_address = '$email'";
+    try {
+        $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// Execute the query
-$result = $conn->query($select_data);
+        $sql = "SELECT * FROM user WHERE email_address = :email";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
 
-if ($result->num_rows > 0) {
-    // Fetch the row as an associative array
-    $row = $result->fetch_assoc();
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    // Verify the password
-    if (password_verify($password, $row['password'])) {
-        // Password is correct
-        print "Login successful!\n";
-    } else {
-        // Password is incorrect
-        print "Incorrect password\n";
+        if ($user) {
+            if (password_verify($password, $user['password'])) {
+                // Password is correct
+                echo "Login successful!\n";
+            } else {
+                // Password is incorrect
+                echo "Incorrect password\n";
+            }
+        } else {
+            // No matching email found
+            echo "Email not found";
+        }
+    } catch (PDOException $e) {
+        echo "Database Error: " . $e->getMessage();
     }
-} else {
-    // No matching email found
-    echo "Email not found";
+
+    $pdo = null;
 }
 ?>
