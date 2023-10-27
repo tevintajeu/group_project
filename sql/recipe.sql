bb6a1993a78b4c54a4270a24de9e41486a49747c
MYNAMEISLOCH <ephraim.juma@strathmore.edu>
2023-10-27 09:23:19 +0300

    create database statement

--- sql/recipe.sql
+++ sql/recipe.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `user`
 --
-
+CREATE DATABASE recipe;
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
