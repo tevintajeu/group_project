418ec3a58729ac2108b842d393d5dfc8e959449c
MYNAMEISLOCH <ephraim.juma@strathmore.edu>
2023-10-27 09:50:33 +0300

    .

--- layout/layout.php
+++ layout/layout.php
@@ -1150,7 +1150,7 @@ public function sign_in()
                         </div>
 
                         <div class="mb-3">
-                          <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/place/Madaraka+Estate,+Nairobi/@-1.3105352,36.8130479,17z/data=!3m1!4b1!4m6!3m5!1s0x182f10f7e6febe2b:0xa7f21387bb85e5a1!8m2!3d-1.3105352!4d36.8130479!16s%2Fg%2F11g0g9kd3h?entry=ttu" frameborder="0" allowfullscreen></iframe>
+                          <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
                         </div><!-- End Google Maps -->
 
                         <div class="row gy-4">
