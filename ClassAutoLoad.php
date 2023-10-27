37861532cec673b6c29a3f19164a429068ee05c7
tevintajeu <tevintajeu>
2023-10-27 09:53:13 +0300

    email verification added

--- ClassAutoLoad.php
+++ ClassAutoLoad.php
@@ -1,8 +1,10 @@
 <?php
+date_default_timezone_set("Africa/Nairobi");
+
 spl_autoload_register('ClassAutoLoad', true, true);
 function ClassAutoLoad($class){
 
-    $directories = array("layout");
+    $directories = array("layout","processes");
     foreach($directories as $dir){
         $filename =  dirname(__FILE__).DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR. $class . ".php";
 
@@ -16,4 +18,5 @@ function ClassAutoLoad($class){
 
 
 $OBJ_Layout = new Layout();
+$OBJ_processes = new processes();
 ?>
